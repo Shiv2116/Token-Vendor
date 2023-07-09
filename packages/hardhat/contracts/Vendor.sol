9b88b1c058e7cdd6e180fb2042c6b072315fb916
Shiv2116 <114937365+Shiv2116@users.noreply.github.com>
2023-07-09 23:41:00 +0530

    First commit

--- packages/hardhat/contracts/Vendor.sol
+++ packages/hardhat/contracts/Vendor.sol
@@ -6,18 +6,37 @@ import "./YourToken.sol";
 
 contract Vendor is Ownable {
 
-  //event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
+  event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
+  event SellTokens(address seller, uint256 amountOfTokens, uint256 amountOfETH);
 
   YourToken public yourToken;
+  uint256 public constant tokensPerEth = 100;
 
   constructor(address tokenAddress) {
     yourToken = YourToken(tokenAddress);
   }
 
   // ToDo: create a payable buyTokens() function:
+  function  buyTokens() public payable {
+    uint256 calculateAmount = tokensPerEth * msg.value;
+    yourToken.transfer(msg.sender,calculateAmount);
+    emit BuyTokens (msg.sender,msg.value,calculateAmount);
+  }
+
 
   // ToDo: create a withdraw() function that lets the owner withdraw ETH
+  function withdraw() public onlyOwner {
+    require(address(this).balance>0,"nothing to withdraw");
+    payable(msg.sender).transfer(address(this).balance);
+  }
 
   // ToDo: create a sellTokens(uint256 _amount) function:
+  function  sellTokens(uint256 _amount) public {
+    require(yourToken.balanceOf(msg.sender)>=_amount,"Insufficient Funds");
 
+    uint256 ethAmount = _amount/tokensPerEth;
+    yourToken.transferFrom(msg.sender,address(this),_amount);
+    payable(msg.sender).transfer(ethAmount);
+     emit SellTokens(msg.sender, _amount, ethAmount);
+  }
 }
